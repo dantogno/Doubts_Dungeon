83a022f0b894c0dfdbbf87fda658cbe9cf3419ac
marpl6967 <72100598+marpl6967@users.noreply.github.com>
2023-10-04 22:25:25 -0500

    Initial Setup Of My Porting Over Of Stuff

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public GameObject GameOverObject;
+    // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
