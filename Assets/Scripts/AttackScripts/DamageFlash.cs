26385b8f094724acda2aeee8f78759f221bf168c
Mack Pearson <76492881+chopov1@users.noreply.github.com>
2023-10-29 17:04:58 -0500

    flash now works with skelly

--- Assets/Scripts/AttackScripts/DamageFlash.cs
+++ Assets/Scripts/AttackScripts/DamageFlash.cs
@@ -4,32 +4,70 @@ using UnityEngine;
 
 public class DamageFlash : MonoBehaviour
 {
+    public enum RenderType { MeshRenderer, SkinnedMeshRenderer}
+
+    public RenderType type;
+
     MeshRenderer meshrenderer;
+    SkinnedMeshRenderer skinnedMeshRenderer;
     [SerializeField]
     float flashTime = .15f;
     [SerializeField]
     Material flashMaterial;
     Material originalMat;
+
     // Start is called before the first frame update
     void Start()
     {
-        meshrenderer = GetComponent<MeshRenderer>();
-        if(meshrenderer == null)
+        switch (type)
         {
-            meshrenderer = GetComponentInChildren<MeshRenderer>();
+            case RenderType.MeshRenderer:
+                meshrenderer = GetComponent<MeshRenderer>();
+                if (meshrenderer == null)
+                {
+                    meshrenderer = GetComponentInChildren<MeshRenderer>();
+                }
+                originalMat = meshrenderer.material;
+                break;
+            case RenderType.SkinnedMeshRenderer:
+                skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+                if (skinnedMeshRenderer== null)
+                {
+                    skinnedMeshRenderer= GetComponentInChildren<SkinnedMeshRenderer>();
+                }
+                originalMat = skinnedMeshRenderer.material;
+                break;
         }
-        originalMat = meshrenderer.material;
     }
 
     public void Flash()
     {
-        meshrenderer.material = flashMaterial;
-        Invoke("flashStop", flashTime);
+        switch (type)
+        {
+            case RenderType.MeshRenderer:
+                meshrenderer.material = flashMaterial;
+                Invoke("flashStop", flashTime);
+                break;
+            case RenderType.SkinnedMeshRenderer:
+                skinnedMeshRenderer.material = flashMaterial;
+                Invoke("flashStop", flashTime);
+                break;
+        }
+        
     }
 
     void flashStop()
     {
-        meshrenderer.material = originalMat;
+        switch (type)
+        {
+            case RenderType.MeshRenderer:
+                meshrenderer.material = originalMat;
+                break;
+            case RenderType.SkinnedMeshRenderer:
+                skinnedMeshRenderer.material = originalMat;
+                break;
+        }
+        
     }
 
     // Update is called once per frame
