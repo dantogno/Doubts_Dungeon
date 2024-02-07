f323fc3636233ec5d92ca9dc11405bd3bf61ff4f
CiarennHollis <94020137+CiarennHollis@users.noreply.github.com>
2024-02-07 16:02:02 -0600

    Adds a new test scene for testing the new player movement script. Adds more code to the new player movement script.

--- Assets/Scripts/Player/PlayerMovementNew.cs
+++ Assets/Scripts/Player/PlayerMovementNew.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum InputMode { MouseAndKeyboard, Controller }
 public class PlayerMovementNew : MonoBehaviour
 {
     // P R O P E R T I E S
-
+    
     [Header("Movement Settings")]
     [SerializeField] float Speed = 8f;
     [SerializeField] float rotationSpeed = 360.0f;
@@ -23,7 +24,7 @@ public class PlayerMovementNew : MonoBehaviour
     float totalShakeTime;
     float shakeTimer;
 
-    CinemachineVirtualCamera CMVcam;
+    
 
     [Header("Animation Settings")]
     [SerializeField] Animator playerAnimator;
@@ -33,16 +34,23 @@ public class PlayerMovementNew : MonoBehaviour
     Plane plane;
 
     [SerializeField]
-    public bool usingController;
+    public InputMode PlayerInputMode;
+
+    [Header("References")]
+    public ShootBehavior SB;
+    private Rigidbody rb;
+    CinemachineVirtualCamera CMVcam;
 
 
     // U N I T Y  M E T H O D S
     // Start is called before the first frame update
     void Start()
     {
-
         plane = new Plane(Vector3.down, transform.position.y);
         CMVcam = VirtualCamera.GetComponent<CinemachineVirtualCamera>();
+        playerAnimator = GetComponent<Animator>();
+        SB = GetComponent<ShootBehavior>(); 
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
@@ -53,24 +61,101 @@ public class PlayerMovementNew : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        Move();
+        HandleLookDirection();
     }
 
 
     // M I S C
 
+    void HandleLookDirection()
+    {
+        switch(PlayerInputMode)
+        {
+            case InputMode.MouseAndKeyboard:
+                LookAtMouse();
+                break;
+
+            case InputMode.Controller:
+
+                break;
+        }
+    }
     void LookAtMouse()
     {
         // turns the player to be looking at the mouse
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            Vector3 targetPosition = ray.GetPoint(distance);
+
+            // Preserve the y-coordinate of the player
+            // Calculate the direction only in the x and z plane, keeping y fixed
+            Vector3 direction = (targetPosition - transform.position);
+            direction.y = 0; // Keep y-coordinate fixed (flat plane)
+
+            if (direction != Vector3.zero)
+            {
+                // Calculate the angle between the forward direction and the mouse direction
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+                // Rotate the player around the Y-axis to face the mouse cursor
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                rb.MoveRotation(Quaternion.Euler(0f, angle, 0f));
+            }
+        }
     }
     void Move()
     {
         // moves the player 
+        Vector3 move = GetMoveVector();
+        playerAnimator.SetFloat("speed", move.magnitude);
+
+        move *= Speed * Time.deltaTime;
+        rb.AddForce(move, ForceMode.Force);
+
     }
 
-    void GetMoveVector()
+    Vector3 GetMoveVector()
     {
         //calculates the move vector for the player 
+
+        Vector3 move = Vector3.zero;
+
+        switch (PlayerInputMode)
+        {
+            case InputMode.MouseAndKeyboard:
+                move = (Input.GetAxis("Vertical") * getCameraForward()
+                    + Input.GetAxis("Horizontal") * getCameraRight()).normalized;
+                break;
+
+            case InputMode.Controller:
+                move = (Input.GetAxis("LSVertical") * getCameraForward()
+                    + Input.GetAxis("LSHorizontal") * getCameraRight()).normalized;
+                break;
+        }
+
+        return move;
+        
+    }
+    Vector3 getCameraForward()
+    {
+        // Calculate movement direction based on camera's perspective
+        Vector3 cameraForward = Camera.main.transform.forward;
+        // Ignore the y-component to stay in the x-z plane
+        cameraForward.y = 0f;
+        cameraForward.Normalize();
+        return cameraForward;
+    }
+    Vector3 getCameraRight()
+    {
+        // Calculate movement direction based on camera's perspective
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        // Ignore the y-component to stay in the x-z plane
+        cameraRight.y = 0f;
+        cameraRight.Normalize();
+        return cameraRight;
     }
 
     void Dodge()
