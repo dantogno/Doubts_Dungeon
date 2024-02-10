b4cb106b4fa6dc8f601f8319df22371297bcbd77
marpl6967 <72100598+marpl6967@users.noreply.github.com>
2024-02-10 08:27:34 -0600

    Damaging surface in

---
+++ Assets/Scripts/DamageSurface.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSurface : MonoBehaviour
+{
+    [SerializeField]
+    float DMGInterval;
+
+    [SerializeField]
+    int Damage;
+
+    [SerializeField]
+    float EnvSpeed;
+
+    [SerializeField]
+    bool CanTakeDamage = true;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && CanTakeDamage)
+        {
+            DamagePlayer(collision);
+        }
+
+        collision.gameObject.GetComponent<PlayerMovement>().Speed = EnvSpeed;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && CanTakeDamage)
+        {
+            DamagePlayer(collision);
+        }
+    }
+    void DamagePlayer(Collision player)
+    {
+        player.gameObject.GetComponent<PlayerHealth>().TakeDamage(Damage);
+        CanTakeDamage = false;
+        StartCoroutine(DamageInterval());
+    }
+    
+    private IEnumerator DamageInterval()
+    {
+        yield return new WaitForSeconds(DMGInterval);
+        CanTakeDamage = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.gameObject.GetComponent<PlayerMovement>().Speed = 8;
+    }
+}
