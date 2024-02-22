13c8d3020509cf0a5fc6622a28b0bdd1309d0da6
Grace Anders <grace.anders2002@gmail.com>
2024-02-22 14:45:56 -0600

    dont destroy partially implimented

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -15,22 +15,22 @@ public class DontDestroy : MonoBehaviour
     void Start()
     {
         //This Enables objects to not duplicate when returning to a previous space
+       // DontDestroy[] protectedObjects = Object.FindObjectsOfType<DontDestroy>();
 
         //For all of the objects that have the DontDestroy Script
-        for(int i =0; i < Object.FindObjectsOfType<DontDestroy>().Length; i++)
+        for (int i =0; i < Object.FindObjectsOfType<DontDestroy>().Length; i++)
         {
             if (Object.FindObjectsOfType<DontDestroy>()[i] != this)
             {
-                Destroy(gameObject);
+                if (Object.FindObjectsOfType<DontDestroy>()[i].objectID == objectID)
+                {
+                    Destroy(gameObject);
+                }
+                
             }
         }
 
         DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
